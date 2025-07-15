54dcf4194f5105da2ea25e741e33b6f3f9668da6
Dinara Urazova <dinara.urazova88@gmail.com>
2025-07-15 20:24:34 +0500

    comment drop_schema to prevent an accident deletion

--- create_tables.py
+++ create_tables.py
@@ -2,7 +2,7 @@ import asyncio
 from bot.db.user_storage import create_schema, drop_schema
 
 async def main() -> None:
-    await drop_schema()
+    # await drop_schema()
     await create_schema()
 
 if __name__ == "__main__":
