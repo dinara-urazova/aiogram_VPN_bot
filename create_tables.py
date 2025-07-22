1b8f4edcf07da068f6727d6d8b416cace2c4d7eb
Dinara Urazova <dinara.urazova88@gmail.com>
2025-07-22 19:21:10 +0500

    add comment

--- create_tables.py
+++ create_tables.py
@@ -3,6 +3,8 @@ from bot.db.db_engine import engine
 from bot.db.base import Base
 from bot.db.models import User, TelegramEvent  # noqa: F401
 
+# we need the line above to create tables in db
+
 
 async def create_schema() -> None:
     async with engine.begin() as conn:
