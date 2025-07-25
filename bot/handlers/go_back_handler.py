41f3674c89fd070167fe67f8e7ef3c16fa9e517b
Oleg Sklyarov <oleg.sklyarov@gmail.com>
2025-07-25 16:23:14 +0300

    AVB-6 remove debug print

--- bot/handlers/go_back_handler.py
+++ bot/handlers/go_back_handler.py
@@ -7,5 +7,4 @@ router = Router()
 @router.callback_query(F.data == "go_back")
 async def go_back_handler(callback: CallbackQuery):
     await callback.answer()
-    print("Нажата кнопка 'Назад'")
     await callback.message.delete()
