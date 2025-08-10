568010a35f41778c66489433bd305ff530090e32
Dinara Urazova <dinara.urazova88@gmail.com>
2025-08-11 02:44:57 +0500

    added new fun (check if the client exists in the inbound list), fix Already_Login bug

--- vpn_client.py
+++ vpn_client.py
@@ -3,49 +3,53 @@ import logging
 import uuid
 from urllib.parse import urlencode, quote
 from pyxui_async import XUI
-from pyxui_async.errors import BadLogin
+from pyxui_async.errors import BadLogin, NotFound
 from bot.config_reader import env_config
 
 xui = XUI(full_address=env_config.panel_url, panel="sanaei", session_string=None)
+is_logged_in = False
 
 
 async def get_client_key(telegram_id):
+    global is_logged_in
     try:
-        # logging.info("Login started")
-        username = env_config.panel_username
-        password = env_config.panel_password.get_secret_value()
-        response = await xui.login(username, password)
-        if response is not True:
-            # logging.error("❌ Login failed")
-            return None
-        # logging.info("🔥 Successful login")
-        # logging.info("Добавляем клиента...")
-        client_uuid = str(uuid.uuid4())
-        add_client_response = await xui.add_client(
-            inbound_id=1,
-            email=telegram_id,
-            uuid=client_uuid,
-            enable=True,
-            flow="xtls-rprx-vision",
-        )
-        if not add_client_response.get("success", False):  # дб "success": True
-            # logging.error(f"❌ Adding client failed")
-            return None
-        # logging.info("✅ Adding client succeeded")
-        # logging.info("🔑 Getting the key")
+        if not is_logged_in: # код выполнится только при False (не залогинен)
+            username = env_config.panel_username
+            password = env_config.panel_password.get_secret_value()
+            response = await xui.login(username, password)
+            if response is not True:
+                logging.error("❌ Login failed")
+                return None
+            is_logged_in = True
+            logging.info("🔥 Successful login")
+        inbound_id = 1
+        try:
+            client = await xui.get_client(inbound_id, email=telegram_id)
+            client_uuid = client["id"]
+        except NotFound: # новый клиент
+            client_uuid = str(uuid.uuid4())
+            add_client_response = await xui.add_client(
+                inbound_id=1,
+                email=telegram_id,
+                uuid=client_uuid,
+                enable=True,
+                flow="xtls-rprx-vision",
+            )
+            if not add_client_response.get("success", False):  # дб "success": True
+                logging.error(f"❌ Adding client failed")
+                return None
         inbound_list = await xui.get_inbound(inbound_id=1)  # функция возвращает словарь
         if not inbound_list.get("success", False):
-            # logging.error(f"❌ Getting the inbound list failed")
+            logging.error(f"❌ Getting the inbound list failed")
             return None
-        # logging.info("❗️ Got the inbound list")
         key_string = await build_vless_key(inbound_list, client_uuid)
         return key_string
 
     except BadLogin:
-        # logging.error("❌ Ошибка авторизации в XUI.")
+        logging.error("❌ Ошибка авторизации")
         return None
     except Exception as e:
-        # logging.error(f"⚠️ Ошибка при получении ключа: {e}")
+        logging.error(f"⚠️ Ошибка при получении ключа: {e}")
         print(f"Error={e}")
         return None
 
@@ -55,16 +59,11 @@ async def build_vless_key(inbound_list: dict, client_uuid):
     settings = json.loads(
         obj["settings"]
     )  # получаем доступ к settings по ключу и парсим их в словарь, так как settings - строка json
-    logging.info(f"Settings={settings}")
     stream_settings = json.loads(obj["streamSettings"])
     clients = settings["clients"]
-    client = None  # можно и одной строкой client = next((c for c in clients if c["id"] == client_uuid), None), next здесь выбирает 1 встретившийся экз (как break), в прот случае - None как в словарном методе get
-    for c in clients:
-        if c["id"] == client_uuid:
-            client = c
-            break
+    client = next((c for c in clients if c["id"] == client_uuid), None) # next здесь выбирает 1 встретившийся экз 
     if client is None:
-        # logging.error("Клиент не найден в списке inbound")
+        logging.error("Клиент не найден в списке inbound")
         return None
 
     config = {
@@ -90,3 +89,6 @@ async def build_vless_key(inbound_list: dict, client_uuid):
     key_string = f"vless://{config['id']}@{config['add']}:{config['port']}?{query_str}#{ps_encoded}"
     logging.info(f"⚡️ Congrats!. Here's the key={key_string}")
     return key_string
+
+
+
