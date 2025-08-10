679d008f8f1bb009dfea8001bdb401704865036d
Dinara Urazova <dinara.urazova88@gmail.com>
2025-08-11 03:06:58 +0500

    format

--- vpn_client.py
+++ vpn_client.py
@@ -13,7 +13,7 @@ is_logged_in = False
 async def get_client_key(telegram_id):
     global is_logged_in
     try:
-        if not is_logged_in: # код выполнится только при False (не залогинен)
+        if not is_logged_in:  # код выполнится только при False (не залогинен)
             username = env_config.panel_username
             password = env_config.panel_password.get_secret_value()
             response = await xui.login(username, password)
@@ -26,7 +26,7 @@ async def get_client_key(telegram_id):
         try:
             client = await xui.get_client(inbound_id, email=telegram_id)
             client_uuid = client["id"]
-        except NotFound: # новый клиент
+        except NotFound:  # новый клиент
             client_uuid = str(uuid.uuid4())
             add_client_response = await xui.add_client(
                 inbound_id=1,
@@ -36,11 +36,11 @@ async def get_client_key(telegram_id):
                 flow="xtls-rprx-vision",
             )
             if not add_client_response.get("success", False):  # дб "success": True
-                logging.error(f"❌ Adding client failed")
+                logging.error("❌ Adding client failed")
                 return None
         inbound_list = await xui.get_inbound(inbound_id=1)  # функция возвращает словарь
         if not inbound_list.get("success", False):
-            logging.error(f"❌ Getting the inbound list failed")
+            logging.error("❌ Getting the inbound list failed")
             return None
         key_string = await build_vless_key(inbound_list, client_uuid)
         return key_string
@@ -61,7 +61,9 @@ async def build_vless_key(inbound_list: dict, client_uuid):
     )  # получаем доступ к settings по ключу и парсим их в словарь, так как settings - строка json
     stream_settings = json.loads(obj["streamSettings"])
     clients = settings["clients"]
-    client = next((c for c in clients if c["id"] == client_uuid), None) # next здесь выбирает 1 встретившийся экз 
+    client = next(
+        (c for c in clients if c["id"] == client_uuid), None
+    )  # next здесь выбирает 1 встретившийся экз
     if client is None:
         logging.error("Клиент не найден в списке inbound")
         return None
@@ -89,6 +91,3 @@ async def build_vless_key(inbound_list: dict, client_uuid):
     key_string = f"vless://{config['id']}@{config['add']}:{config['port']}?{query_str}#{ps_encoded}"
     logging.info(f"⚡️ Congrats!. Here's the key={key_string}")
     return key_string
-
-
-
