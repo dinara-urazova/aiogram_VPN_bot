5afd2487e1cf78d86b678acd7e6f5627984e9506
Dinara Urazova <dinara.urazova88@gmail.com>
2025-09-05 18:43:35 +0500

    fix db bug

--- bot/db/database.py
+++ bot/db/database.py
@@ -31,7 +31,7 @@ async def get_user_by_telegram_id(telegram_id: int) -> User | None:
 async def get_expired_vpn_users() -> Sequence[User]:
     async with AsyncSession(bind=engine, autoflush=False) as async_session:
         statement = select(User).where(
-            User.expires_at <= datetime.now(timezone.utc), User.is_vpn_enabled
+            User.expires_at <= datetime.now(timezone.utc), User.is_3x_ui_key_enabled
         )
         result = await async_session.execute(statement)
         return result.scalars().all()
@@ -118,7 +118,7 @@ async def enable_vpn_in_db(telegram_id: int) -> None:
         statement = (
             update(User)
             .where(User.telegram_id == telegram_id)
-            .values(is_vpn_enabled=True)
+            .values(is_3x_ui_key_enabled=True)  
         )
         await async_session.execute(statement)
         await async_session.commit()
@@ -129,7 +129,7 @@ async def disable_vpn_in_db(telegram_id: int) -> None:
         statement = (
             update(User)
             .where(User.telegram_id == telegram_id)
-            .values(is_vpn_enabled=False)
+            .values(is_3x_ui_key_enabled=False)
         )
         await async_session.execute(statement)
         await async_session.commit()
